53d3c346c6f8e1802572b2c4e0164db43397b315
Gabriel Arturo Urena <gurena@intellisys.com.do>
2020-04-18 19:03:47 -0400

    ✨ adds new apps to brew script

--- src/brew.sh
+++ src/brew.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# brew cask
+# https://caskroom.github.io/search
 brew tap caskroom/cask
 
 # Dev Tools
@@ -8,17 +10,21 @@ brew install yarn
 brew cask install iterm2
 brew install docker   
 brew cask install postman
-brew cask install the-unarchiver
 
 # Communication Apps
 brew cask install slack
+brew cask install discord
 
 # Web tools
 brew cask install google-chrome
 
 # File Storage
 brew cask install dropbox
+brew cask install google-drive
 
 # Entertainment
 brew cask install spotify
 
+# General
+brew cask install the-unarchiver
+brew cask install flux
