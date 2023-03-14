7afc17b2f4fecf3b2a6127043b8307288bc6837d
Gabriel Urena <gabriel.urena@hearst.com>
2023-03-14 00:42:38 -0400

    updates homebrew install script

--- src/brew.sh
+++ src/brew.sh
@@ -2,31 +2,51 @@
 
 # brew cask
 # https://caskroom.github.io/search
-brew tap caskroom/cask
+brew tap homebrew/cask 
 
 # Dev Tools
 brew install git
 brew install yarn
-brew cask install iterm2
+brew install --cask iterm2
 brew install docker   
-brew cask install postman
-brew cask install ngrok
+brew install --cask postman
+brew install cloudflared 
+brew install --cask ngrok
+brew install --cask visual-studio-code 
+brew install python3
+brew install awscli 
+brew install pyenv 
+brew install neovim
+brew install tmux
+brew install openvpn
+brew install rust
+brew install --cask sdm
+brew install --cask krisp
+brew install --cask datagrip
 
 # Communication Apps
-brew cask install slack
-brew cask install discord
+brew install --cask slack
+brew install --cask discord
+brew install --cask telegram
 
 # Web tools
-brew cask install google-chrome
+brew install --cask google-chrome
+brew install --cask brave-browser
+brew install --cask vivaldi
 
 # File Storage
-brew cask install dropbox
-brew cask install google-drive
+brew install --cask dropbox
 
 # Entertainment
-brew cask install spotify
+brew install --cask spotify
+brew install --cask stremio
+brew install spotifyd
+brew install spotify-tui
 
 # General
-brew cask install the-unarchiver
-brew cask install flux
-brew cask install spectacle
+brew install --cask the-unarchiver
+brew install --cask flux
+brew install --cask rectangle 
+brew install --cask nord-vpn
+brew install --cask android-file-transfer
+brew install --cask daisydisk
